ca927806cd1d764a8f844dd60e3ab884b2cd3986
denielll <deniel3678@gmail.com>
2019-10-18 02:12:08 -0400

    [feature] Add heuristics

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import math
+
 class Model:
     def predict():
         pass
@@ -21,3 +23,49 @@ class Jaccard(Model):
         int_count = len(n1.intersection(n2))
         union_count = len(n1.union(n2))
         return int_count / union_count
+
+# the sum of the inverse log of the degree of common neighbors. commonly used for social network
+class AdamicAdar(Model):
+    def name():
+        return 'Adamic Adar'
+
+    def predict(G, node1, node2):
+        n1 = set(G.neighbors(node1))
+        n2 = set(G.neighbors(node2))
+        cn_list = list(n1.intersection(n2))
+        tmp = []
+        for i in cn_list:
+            tmp.append(1/math.log(len(G.neighbors(i))))
+        return sum(tmp)
+
+# the product of the degree of each node
+class PreferentialAttachment(Model):
+    def name():
+        return 'Preferential Attachment'
+
+    def predict(G, node1, node2):
+        n1 = set(G.neighbors(node1))
+        n2 = set(G.neighbors(node2))
+        return len(n1)*len(n2)
+
+# total unique neighbors of the two nodes
+class TotalNeighbors(Model):
+    def name():
+        return 'Total Neighbors'
+
+    def predict(G, node1, node2):
+        n1 = set(G.neighbors(node1))
+        n2 = set(G.neighbors(node2))
+        return len(n1.union(n2))
+
+        
+
+
+
+
+
+
+
+
+
+
